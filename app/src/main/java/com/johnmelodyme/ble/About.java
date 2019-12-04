38f30afb4c7686910c9ea06c5e0cfee371751fc1
John Melody Melissa <Johnmelodyme@yandex.com>
2019-12-04 16:16:06 +0800

    Updated

--- app/src/main/java/com/johnmelodyme/ble/About.java
+++ app/src/main/java/com/johnmelodyme/ble/About.java
@@ -1,9 +1,18 @@
 package com.johnmelodyme.ble;
-
+/**
+ * @CREATOR: JOHN MELODY MELISSA ESKHOLAZHT .C.T.K.
+ * @DATETIME: 12/12/2019
+ * @COPYRIGHT: 2019 - 2023
+ * @PROJECTNAME: BLUETOOTH LOW ENERGY TUTORIAL
+ */
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Context;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.ListView;
 
@@ -11,25 +20,28 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class About extends AppCompatActivity {
-
-    ListView listview_about;
-
+    ListView listView;
+    String [] about = {"About", "Version", "Source"};
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
 
-        listview_about = findViewById(R.id.about_list_view_main);
-        String[] about_about;
-        about_about = new String[]{"About", "Version", "Source"};
+        listView = findViewById(R.id.about_list_view_main);
+        ArrayAdapter<String> stringArrayAdapter;
+        stringArrayAdapter = new ArrayAdapter<String>(About.this, R.layout.support_simple_spinner_dropdown_item, about);
+        listView.setAdapter(stringArrayAdapter);
 
-        final ArrayList<String> list = new ArrayList<String>();
-        for (int i =0; i < about_about.length; ++i){
-            list.add(about_about[i]);
-        }
-
-        ArrayAdapter<String> arrayAdapter;
-        arrayAdapter = new ArrayAdapter<>(this, R.layout.about_list, list);
-
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Intent source;
+                String url;
+                url = "https://github.com/johnmelodyme/Bluetooth";
+                source = new Intent(Intent.ACTION_VIEW,
+                        Uri.parse(url));
+                startActivity(source);
+            }
+        });
     }
 }
